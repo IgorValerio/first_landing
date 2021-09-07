df17bd93fa88b8d0ad4d8fec3e05c8e126f3d2d8
Igor <igorv386@gmail.com>
2021-09-07 16:30:46 -0300

    Commit inicial

---
+++ src/Views/header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE HTML>
+<html lang="pt-br">
+<head>
+    <meta charset="utf-8"/>
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1" name="viewport">
+    <title>Base Landing Page</title>
+    <link rel="shortcut icon" href="#"> 
+    <link href="<?=URL_BASE?>resources/css/css.css" rel="stylesheet"/>
+</head>
+<body>
+	<header>
+		<div class="container">
+        <div class="logo">
+            <img src="<?=URL_BASE?>resources/imagens/logo.png">
+        </div>
+        <div class="menus">
+            <div class="menu">
+              
+            </div>
+            <div class="social">
+
+              </div>
+        </div>
+    </div>
+	</header>
