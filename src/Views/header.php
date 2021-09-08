db0ad446f75332dd8d1f0b20b7483581649c519c
Igor <igorv386@gmail.com>
2021-09-08 19:21:31 -0300

    Estilizaçao do menu

--- src/Views/header.php
+++ src/Views/header.php
@@ -6,20 +6,30 @@
     <title>Base Landing Page</title>
     <link rel="shortcut icon" href="#"> 
     <link href="<?=URL_BASE?>resources/css/css.css" rel="stylesheet"/>
+    <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css"> -->
+    <script src="https://kit.fontawesome.com/1e24cdf620.js" crossorigin="anonymous"></script>
 </head>
 <body>
 	<header>
 		<div class="container">
         <div class="logo">
-            <img src="<?=URL_BASE?>resources/imagens/logo.png">
+            <img src="<?=URL_BASE?>resources/imagens/logo.jpeg">
         </div>
         <div class="menus">
             <div class="menu">
-              
+              <ul>
+                <li><a href="#">Quem sou eu</a></li>
+                <li><a href="#">Eventos</a></li>
+                <li><a href="#">Cursos</a></li>
+                <li><a href="#">Videos</a></li>
+                <li><a href="#">Fale Comigo</a></li>
+              </ul>
             </div>
             <div class="social">
-
-              </div>
+              <span>Siga-me: </span>
+              <a href="#"><i class="fab fa-facebook-f" style="font-size: 1.2rem; color: #FFF;"></i></a>
+              <a href="#"><i class="fab fa-instagram" style="font-size: 1.5rem; color: #FFF;"></i>
+            </div>
         </div>
     </div>
 	</header>
