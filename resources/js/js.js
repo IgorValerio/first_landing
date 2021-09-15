67e4384dcd050cd8dd58f2d3f8ad8298ac6840cb
Igor <igorv386@gmail.com>
2021-09-15 16:25:48 -0300

    corrigindo padding mobile

--- resources/js/js.js
+++ resources/js/js.js
@@ -1,3 +1,8 @@
 $(document).ready(function(){
-
+    $('.btn-menu').on('click', function(){
+      alert('teste');
+    });
+    $('.menus').before().on('click', function(){
+      alert('teste');
+    });
 });
