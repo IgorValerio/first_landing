b4aa48c8bc87c8773aa476abe4fa38e47be463a6
Igor <igorv386@gmail.com>
2021-09-16 18:32:56 -0300

    criando estilo da home

--- src/Views/home.php
+++ src/Views/home.php
@@ -1,5 +1,33 @@
 <?=$this->fetch('header.php')?>
-<?php
-	echo 'Hello Word<br/>';
-?>
+
+
+<section class="quem_sou_eu">
+      <div class="container">
+        <div class="conteudo">
+            <img src="<?=URL_BASE?>resources/imagens/tonho.jpg">
+            <div class="texto">
+              <h1 class="titulo">Quem sou eu?</h1>
+              <div class="descricao">
+                <p>
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloremque aliquam quibusdam nobis ut maxime,
+                 ab molestiae fugit reprehenderit odit cum totam magnam nulla impedit minus ex, distinctio, eos blanditiis rerum.
+                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid corrupti tenetur explicabo rem, harum odio illo!
+                </p>
+                
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam soluta quos inventore itaque est porro nemo. 
+                 Suscipit dolore ipsa ducimus? Consequatur, a! Sapiente nostrum perferendis reiciendis ratione quibusdam a qui.
+                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere veritatis delectus nesciunt architecto quo totam.
+                </p>
+                 
+              </div>
+              <a href="#" class="btn-principal">Veja o video</a>
+            </div> 
+        </div>
+      </div>
+</section>
+
+
+
+
 <?=$this->fetch('footer.php')?>
