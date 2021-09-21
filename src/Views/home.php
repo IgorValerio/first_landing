6004a3baab5b4103236002d6bb888e54caf47389
Igor <igorv386@gmail.com>
2021-09-21 19:18:58 -0300

    Criar estrutura HTML do bloco Nossos Treinamentos

--- src/Views/home.php
+++ src/Views/home.php
@@ -33,27 +33,25 @@
           <h2 class="titulo">Eventos</h2>
           <div class="lista">
             <div class="item">
-                <img src="<?=URL_BASE?>resources/imagens/evento-01.jpg">
+              <img src="<?=URL_BASE?>resources/imagens/evento-01.jpg">
                 <div class="texto">
                   <h3 class="titulo">Titulo do Evento 1 vem aqui</h3>
                   <div class="descricao">
                     <p>
                     Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloremque aliquam quibusdam nobis ut maxime,
                     ab molestiae fugit reprehenderit odit cum totam magnam nulla impedit minus ex, distinctio, eos blanditiis rerum.
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid corrupti tenetur explicabo rem, harum odio illo!
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
                     </p>
                     <p>
                       Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora quas delectus a accusamus architecto consequuntur
                     adipisci facilis exercitationem magni reiciendis aperiam suscipit odit, quidem nemo quo! Eos quia facere corporis?
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, vel explicabo. Repellat aperiam dolore id odio ducimus at,
-                    quia, iste quibusdam reprehende. Debitis!
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, vel explicabo. Repellat aperiam dolore id odio ducimus at.
                     </p>
                     
                     <p>
                       Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae, voluptas sapiente debitis, quasi qui neque ex itaque excepturi
                     autem culpa nihil aspernatur soluta enim nisi cumque modi ducimus rerum. Aliquid. Lorem ipsum dolor sit amet consectetur adipisicing
-                    elit. Adipisci maxime deleniti non ipsa quo perferendis accusamus ab reiciendis, quidem odio illum totam soluta! Earum alias, ab saepe
-                    quasi quisquam impedit!
+                    elit. Adipisci maxime deleniti non ipsa quo perferendis accusamus ab reiciendis, quidem odio illum totam soluta! Earum alias, ab saepe.
                     </p>
                     
                   </div>
@@ -61,7 +59,7 @@
             </div>
 
             <div class="item">
-                <img src="<?=URL_BASE?>resources/imagens/evento-02.jpg">
+              <img src="<?=URL_BASE?>resources/imagens/evento-02.jpg">
                 <div class="texto">
                   <h3 class="titulo">Titulo do Evento 2 vem aqui</h3>
                   <div class="descricao">
@@ -92,5 +90,69 @@
     </div>
 </section>
 
+<section class="nossos-cursos">
+    <div class="container">
+        <div class="conteudo">
+          <h2 class="titulo">Nossos Cursos</h2>
+          <div class="lista">
+            <div class="item">
+              <img src="<?=URL_BASE?>resources/imagens/teach-cat.jpg">
+                <div class="texto">
+                  <h3 class="titulo">Curso 1 - Nome do Treinamento</h3>
+                  <div class="descricao">
+                    <p>
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloremque aliquam quibusdam nobis ut maxime,
+                    ab molestiae fugit reprehenderit odit cum totam magnam nulla impedit minus ex, distinctio, eos blanditiis rerum.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid corrupti tenetur explicabo rem, harum odio illo!
+                    </p>
+                    <p>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora quas delectus a accusamus architecto consequuntur
+                    adipisci facilis exercitationem magni reiciendis aperiam suscipit odit, quidem nemo quo! Eos quia facere corporis?
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, vel explicabo. Repellat aperiam dolore id odio ducimus at,
+                    quia, iste quibusdam reprehende. Debitis!
+                    </p>
+                    
+                    <p>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae, voluptas sapiente debitis, quasi qui neque ex itaque excepturi
+                    autem culpa nihil aspernatur soluta enim nisi cumque modi ducimus rerum. Aliquid. Lorem ipsum dolor sit amet consectetur adipisicing
+                    elit. Adipisci maxime deleniti non ipsa quo perferendis accusamus ab reiciendis, quidem odio illum totam soluta! Earum alias, ab saepe
+                    quasi quisquam impedit!
+                    </p>
+                    
+                  </div>
+                </div>
+            </div>
+
+            <div class="item">
+              <img src="<?=URL_BASE?>resources/imagens/teach2-cat.jpg">
+                <div class="texto">
+                  <h3 class="titulo">Curso 2 - Nome do Treinamento</h3>
+                  <div class="descricao">
+                    <p>
+                      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloremque aliquam quibusdam nobis ut maxime,
+                      ab molestiae fugit reprehenderit odit cum totam magnam nulla impedit minus ex, distinctio, eos blanditiis rerum.
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid corrupti tenetur explicabo rem, harum odio illo!
+                      </p>
+                    <p>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora quas delectus a accusamus architecto consequuntur
+                      adipisci facilis exercitationem magni reiciendis aperiam suscipit odit, quidem nemo quo! Eos quia facere corporis?
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, vel explicabo. Repellat aperiam dolore,
+                      . Debitis!
+                    </p>
+                    
+                    <p>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae, voluptas sapiente debitis, quasi qui neque ex itaque excepturi
+                      autem culpa nihil aspernatur soluta enim nisi cumque modi ducimus rerum. Aliquid. Lorem ipsum dolor sit amet consectetur adipisicing
+                      elit. Adipisci maxime deleniti non ipsa quo perferendis accusamus ab reiciendis, quidem odio illum totam soluta! Earum alias, ab saepe
+                      quasi quisquam impedit!
+                    </p>
+                    
+                  </div>
+                </div>
+            </div>
+          </div>
+        </div>
+    </div>
+</section>
 
 <?=$this->fetch('footer.php')?>
